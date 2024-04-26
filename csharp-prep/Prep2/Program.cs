3437e200e94dfa9a24b4b0caa4bd861c4433672c
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2024-04-26 15:01:19 -0600

    Finished prep two assinment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade in the class? ");
+        string StrGrade = Console.ReadLine();
+        int NumGrade = int.Parse(StrGrade);
+
+        string LetterGrade = "?";
+
+        if (NumGrade >= 90)
+        {
+            LetterGrade = "A";
+        }
+        else if (NumGrade >= 80)
+        {
+            LetterGrade = "B";
+        }
+        else if (NumGrade >= 70)
+        {
+            LetterGrade = "C";
+        }
+        else if (NumGrade >= 60)
+        {
+            LetterGrade = "D";
+        }
+        else if (NumGrade < 60)
+        {
+            LetterGrade = "F";
+        }
+
+        Console.WriteLine($"Your Grade is a {LetterGrade}");
+
+        if (NumGrade >= 70)
+        {
+            Console.WriteLine("You Passed the Class!!");
+        }
+        else
+        {
+            Console.WriteLine("Better Luck Next Time");
+        }
+        
     }
 }
