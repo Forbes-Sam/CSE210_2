701a5b5445b0ed344c1ca37b98664dc152d51867
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2024-04-29 17:17:27 -0600

    Finished prep 3 assinment didnt do any stretch challanges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // gets the magic number from the user
+        //Console.Write("What is the magic number? ");
+        //string sfMagicStr = Console.ReadLine();
+        //int sfMagicNum =int.Parse(sfMagicStr);
+
+        Random random = new Random();
+        int sfMagicNum = random.Next(1,101);
+
+        //starts the loop until the user guesses the magic number
+        int sfUserNum = 0;
+        while (sfUserNum != sfMagicNum){
+            Console.Write("What is your guess? ");
+            string sfUserStr = Console.ReadLine();
+            sfUserNum = int.Parse(sfUserStr);
+
+            // Tells the user if they are higher or lower than the guess or if they guessed it
+            if (sfUserNum > sfMagicNum){
+                Console.WriteLine("Lower");
+            }
+            else if (sfUserNum < sfMagicNum){
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("You guessed it!!!");
+            }
+        }
     }
 }
