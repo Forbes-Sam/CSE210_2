6d4ed73fcaa7485a26e9ef7958e83a592338d026
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2024-05-03 13:28:13 -0600

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
+    static void sfDisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string sfPromptUserName ()
+    {
+        string sfName;
+        Console.Write("Please enter your Name: ");
+        sfName = Console.ReadLine();
+        return sfName;
+    }
+
+    static int sfPromptUserNumber ()
+    {
+        string sfNumStr;
+        int sfNum;
+        Console.Write("Please enter your favorite number: ");
+        sfNumStr = Console.ReadLine();
+        sfNum = int.Parse(sfNumStr);
+        return sfNum;
+    }
+
+    static int sfSquareNumber (int sfNum)
+    {
+        int sfNumReturn;
+        sfNumReturn = sfNum * sfNum;
+        return sfNumReturn;
+    }
+
+    static void sfDisplayResult (string sfName, int sfNum )
+    {
+        Console.WriteLine($"{sfName}, the square of your number is {sfNum}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string sfUser;
+        int sfNum;
+
+        sfDisplayWelcome();
+        sfUser = sfPromptUserName();
+        sfNum = sfPromptUserNumber();
+        sfNum = sfSquareNumber(sfNum);
+        sfDisplayResult(sfUser,sfNum);
+
     }
 }
